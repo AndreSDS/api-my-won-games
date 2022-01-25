c43711e1ef9a9ca6d93e8519c6816152739886f1
AndreSDS <andre_smiths@outlook.com>
2022-01-25 17:02:03 -0300

    implementando método para popular games - createGames

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -50,9 +50,9 @@ async function createManyToManyData(products) {
       });
 
     supportedOperatingSystems &&
-    supportedOperatingSystems.forEach((item) => {
-      platforms[item] = true;
-    });
+      supportedOperatingSystems.forEach((item) => {
+        platforms[item] = true;
+      });
     publishers[publisher] = true;
     developers[developer] = true;
 
@@ -65,6 +65,40 @@ async function createManyToManyData(products) {
   });
 }
 
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, "game");
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.services.game.create({
+          name: product.title,
+          slug: product.slug.replace(/_/g, "-"),
+          price: product.price.amount,
+          release_date: new Date(
+            Number(product.globalReleaseDate) * 1000
+          ).toISOString(),
+          categories: await Promise.all(
+            product.genres.map((name) => getByName(name, "category"))
+          ),
+          platforms: await Promise.all(
+            product.supportedOperatingSystems.map((name) =>
+              getByName(name, "platform")
+            )
+          ),
+          developers: [await getByName(product.developer, "developer")],
+          publishers: await getByName(product.publisher, "publisher"),
+          ...(await getGameInfo(product.slug)),
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -74,7 +108,6 @@ module.exports = {
     } = await axios.get(gogApiUrl);
 
     await createManyToManyData(products);
-    await create(products[2].publisher, "publisher");
-    await create(products[2].developer, "developer");
+    await createGames(products);
   },
 };
